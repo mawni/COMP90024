3dd553002ee9ce7150c333237373a7941f8f83de
Mustafa Awni <mustafaoawni@gmail.com>
2021-04-10 03:33:05 +1000

    finished parsing of the twitter JSON

--- twitterAnalytics.py
+++ twitterAnalytics.py
@@ -11,6 +11,7 @@ for line in words_file:
 #print(sentimentDict['lol'])
 
 
+
 # Make dictionary for the grid info 
 grid_json = open('melbGrid.json')
 grid_dict = json.load(grid_json) #this makes a dictionary out of the json
@@ -20,25 +21,20 @@ grid_dict = json.load(grid_json) #this makes a dictionary out of the json
 
 # for i in grid_dict['features']:
 #     print(i['properties']['id'] + "\n")
-    
+ 
+
+
 # Make list for the twitter info 
 twt_json = open('tinyTwitter.json', encoding='utf-8')
 data = json.load(twt_json) #this makes a dictionary out of the json
 twt_arr = []
 for i in data["rows"]:
-    # print(i["id"])
-    twt_dict[]
-    
-    # we only want to use the tweet text and location (lat,long)
-    
-#print(data["rows"])
-    #twt_dict = {}
-    
-#print(json.dumps(tweets_dict, indent = 4, sort_keys=True))
-# for i in tweets_dict:
-#     print(i)
-    
-    
-    
-    
-    
+    twt_arr.append([i["value"]["geometry"]["coordinates"][0], i["value"]["geometry"]["coordinates"][1], i["doc"]["text"]])
+    # only take in the latitude, longitude, and tweet text. Put into array
+    # note that text for tweets is stored twice. Other location is i["value"]["properties"]["text"]
+#twt_arr[[lat,long,text],..[]]
+
+# conduct analysis on each tweet
+for i in twt_arr:
+    1==1
+print("\n" + "number of tweets = " + str(len(twt_arr)))
