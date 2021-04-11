a6f9779685fda5ab5c9cf045ba14677d24ccb6cf
Mustafa Awni <mustafaoawni@gmail.com>
2021-04-11 15:21:17 +1000

    added function to get the grid a tweet is in (no edge cases considered)

--- twitterAnalytics.py
+++ twitterAnalytics.py
@@ -2,7 +2,8 @@ import json, ijson
 #from mpi4py import MPI
 
 
-# make dictionary of words and their scores from AFINN.txt
+# Make dictionary of words and their scores from AFINN.txt
+
 words_file = open('AFINN.txt','r').readlines()
 sentimentDict = {}
 for line in words_file:
@@ -11,7 +12,10 @@ for line in words_file:
 #print(sentimentDict['lol'])
 
 
-# Make 2d list for the grid info 
+
+
+# Make 2d list for the grid info
+
 grid_json = open('melbGrid.json')
 data1 = json.load(grid_json) #make JSON object from file: key/value pairs
 grid_arr = []
@@ -25,10 +29,12 @@ for i in data1["features"]:
 #     print("\n")
 
 
+
 # Tweets analysed tweet by tweet
+
 twt_json = open('tinyTwitter.json', encoding='utf-8')
-data2 = json.load(twt_json) #make JSON object: key/value pairs
-coordinates = []
+data2 = json.load(twt_json) # make JSON object: key/value pairs
+coordinates = [] # [lat,long]
 text = []
 for i in data2["rows"]:
     coordinates = i["value"]["geometry"]["coordinates"]
@@ -39,3 +45,33 @@ for i in data2["rows"]:
     # print("\n")
     # note that text for tweets is stored twice. Other location is i["value"]["properties"]["text"]
     # the one I chose to parse is cleaner
+    
+    # check which grid
+    grid_index = check_grid(coordinates, grid_arr)
+    
+    # check the words
+    
+    
+    grid_arr[grid_index][5] += 1 #increment total tweets
+    # update grid totalScore
+    
+############## I HAVEN"T HANDLED THE EDGE CASES################
+def check_grid(coordinates, grid_arr):
+    
+    #grid_arr is [[id, xmin, xmax, ymin, ymax, totalTweets, totalScore],..,[]]
+    #coordinates is [lat,long]
+    for index, val in enumerate(grid_arr):
+        if val[1] <= coordinates[0] and coordinates[0] <= val[2]:
+            # the latitude is within range
+            if val[3] <= coordinates[1] and coordinates[1] <= val[4]:
+                # the longitude is within range
+                return index 
+                # grid found
+
+
+
+
+
+
+
+
