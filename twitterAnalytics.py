e1857e95538118ffdfa56424b40940f7b9a178fc
Mustafa Awni <mustafaoawni@gmail.com>
2021-04-11 14:16:11 +1000

    made array for grid json and twitter json

--- twitterAnalytics.py
+++ twitterAnalytics.py
@@ -1,4 +1,4 @@
-import re, json, operator
+import json, ijson
 #from mpi4py import MPI
 
 
@@ -12,29 +12,36 @@ for line in words_file:
 
 
 
-# Make dictionary for the grid info 
+# Make 2d list for the grid info 
 grid_json = open('melbGrid.json')
-grid_dict = json.load(grid_json) #this makes a dictionary out of the json
-# { "type": "Feature"
-#   "properties": { "id": "A1", "xmin": 144.700000, "xmax": 144.850000, "ymin": -37.650000, "ymax": -37.500000 }
-#   "geometry": { "type": "Polygon", "coordinates": [ [ [ 144.7, -37.5 ], [ 144.85, -37.5 ], [ 144.85, -37.65 ], [ 144.7, -37.65 ], [ 144.7, -37.5 ] ] ] } },
+data1 = json.load(grid_json) #make JSON object from file: key/value pairs
+grid_arr = []
+for i in data1["features"]:
+    grid_arr.append([i["properties"]["id"], i["properties"]["xmin"], i["properties"]["xmax"], i["properties"]["ymin"], i["properties"]["ymax"]])
+# grid_arr is [[id, xmin, xmax, ymin, ymax],..,[]]
 
-# for i in grid_dict['features']:
-#     print(i['properties']['id'] + "\n")
- 
+# test print
+# for i in grid_arr:
+#     print(i)
+#     print("\n")
 
 
-# Make list for the twitter info 
+# Make 2d list for twitter info 
 twt_json = open('tinyTwitter.json', encoding='utf-8')
-data = json.load(twt_json) #this makes a dictionary out of the json
+data2 = json.load(twt_json) #make JSON object: key/value pairs
 twt_arr = []
-for i in data["rows"]:
+for i in data2["rows"]:
     twt_arr.append([i["value"]["geometry"]["coordinates"][0], i["value"]["geometry"]["coordinates"][1], i["doc"]["text"]])
-    # only take in the latitude, longitude, and tweet text. Put into array
     # note that text for tweets is stored twice. Other location is i["value"]["properties"]["text"]
-#twt_arr[[lat,long,text],..[]]
+    # the one I chose to parse is cleaner
+#twt_arr is [[lat,long,text],..,[]]
+
+# test print
+# for i in twt_arr:
+#     print(i)
+#     print("\n")
+# print("\n" + "number of tweets = " + str(len(twt_arr)))
 
 # conduct analysis on each tweet
 for i in twt_arr:
     1==1
-print("\n" + "number of tweets = " + str(len(twt_arr)))
